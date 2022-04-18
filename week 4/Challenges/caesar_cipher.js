78bbb61f5ec1d42fbb0bb5597e75bcf8081e7543
Matthew Sievers <MatterSievers@gmail.com>
2022-04-17 23:17:06 -0500

    caesar ciphered

--- week 4/Challenges/caesar_cipher.js
+++ week 4/Challenges/caesar_cipher.js
@@ -4,3 +4,41 @@
 // In the case of a rotation by 3, w, x, y and z would map to z, a, b and c.
 // Input:
 // The unencrypted string s, and k, the number of letters to rotate the alphabet by.
+
+function caesarCipher(s, k) {
+    // Write your code here
+    let alphaSet = 'abcdefghijklmnopqrstuvwxyz';
+    let cipheredMessage = '';
+    for(let i=0; i< s.length; i++){
+        let char = s[i];
+        if (!alphaSet.includes(char.toLowerCase())){
+            cipheredMessage = cipheredMessage.concat(char);
+        } else if(alphaSet.includes(char)){
+            let ciphCharIndex = (alphaSet.indexOf(char) + k) % 26;
+            let ciphChar = alphaSet[ciphCharIndex];
+            cipheredMessage = cipheredMessage.concat(ciphChar); 
+        } else {
+            let ciphCharIndex = (alphaSet.indexOf(char.toLowerCase()) + k) % 26;
+            let ciphChar = alphaSet[ciphCharIndex].toUpperCase();
+            cipheredMessage = cipheredMessage.concat(ciphChar); 
+        }
+    }
+    return cipheredMessage;
+}
+
+let s = 'middle-Outz';
+let k = 2;
+console.log("Test Case 5: Move", s, "by ", k, "spaces.");
+console.log(caesarCipher(s,k),"\n");
+
+s = 'Always-Look-on-the-Bright-Side-of-Life';
+k = 5;
+console.log("Test Case 11: Move", s, "by ", k, "spaces.");
+console.log(caesarCipher(s,k));
+
+// A bit of a warning because the only punctuation the examples and Sample Test Cases had were underscores 
+// until you submit and test it against the other Test Cases.  
+s = 'Hello_World!';
+k = 4;
+console.log("Test Case 0: Move", s, "by ", k, "spaces.");
+console.log(caesarCipher(s,k));
--- week 4/Challenges/minimum_absolute_difference_in_an_array.js
+++ week 4/Challenges/minimum_absolute_difference_in_an_array.js
@@ -2,3 +2,25 @@
 // is written |a-b| or |b-a| and they are equal. If a = 3 and b = 2, |3-2|=|2-3|=1. 
 // Given an array of integers, find the minimum absolute difference between any two elements in the array.
 
+function minimumAbsoluteDifference(arr) {
+    // Write your code here
+    arr.sort((a,b)=> a-b);
+    let minDiff = Math.abs(arr[1]-arr[0]);
+    for(let i=1; i<arr.length-1; i++){
+        let diff = Math.abs(arr[i+1]-arr[i]);
+        minDiff = Math.min(minDiff,diff);
+    }
+    return minDiff;
+}
+
+const A=[3,-7,0];
+console.log("Sample Test Case 0: ", A);
+console.log("Min Abs Diff: ", minimumAbsoluteDifference(A),"\n");
+
+const B=[-59, -36, -13, 1, -53, -92, -2, -96, -54, 75];
+console.log("Sample Test Case 1: ", B);
+console.log("Min Abs Diff: ", minimumAbsoluteDifference(B),"\n");
+
+const C=[1, -3, 71, 68, 17];
+console.log("Sample Test Case 2: ", C);
+console.log("Min Abs Diff: ", minimumAbsoluteDifference(C));
